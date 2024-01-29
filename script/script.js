d118bd91013f9767947232371438809333674055
mouhandarab <behwolf@gmail.com>
2024-01-29 10:14:16 +0100

    2

--- script/script.js
+++ script/script.js
@@ -1,3 +1,9 @@
 console.log('Log, ANESRIF');
 console.error('erreur, ANESRIF');
 console.warn('warning, ANESRIF');
+
+
+const n="mouh";
+const age=27;
+
+console.log("his name is " +n + "and age is "+age);
